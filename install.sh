0116c7044e98401e34fb0f37543fcafa6ce15316
Yuki Tsuboi <y.t.1985.11.7@gmail.com>
2024-06-13 10:58:00 +0900

    [add] starship settings added

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/config/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/config/zsh/.zshenv ~/.zshenv
 mkdir -p ~/.config/sheldon
 ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sf ~/dotfiles/config/starship/starship.toml ~/.config/starship.toml
