a4941933dc0cf5aee34b44d964b87cd0450582c2
Yuki Tsuboi <y.t.1985.11.7@gmail.com>
2024-06-13 10:29:11 +0900

    [update] sync.zsh updated

--- config/zsh/sync.zsh
+++ config/zsh/sync.zsh
@@ -1,3 +1,10 @@
 # Setup a configuration that requires synchronization
 
+## zsh
+### Keep lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=10000
+SAVEHIST=100000
+HISTFILE=~/.zsh_history
 setopt histignorealldups sharehistory
+
+
